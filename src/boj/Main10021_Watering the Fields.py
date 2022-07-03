95176e329e4228027936a909595526ec204d100c
korjun1993 <corojoon93@naver.com>
2022-07-04 03:31:32 +0900

    [추가] 코딩테스트 스터디 - Watering ther Fields (Prim-Algorithm 풀이 -> 메모리 초과 해결)

--- src/boj/Main10021_Watering the Fields.py
+++ src/boj/Main10021_Watering the Fields.py
@@ -1,45 +1,40 @@
-from itertools import combinations
+import heapq
 
 # input
-N, C = map(lambda x: int(x), input().split())
-vertex = [tuple(map(lambda x: int(x), input().split())) for _ in range(N)]
-edges = []
-
-# union-find
-parent = [i for i in range(N)]
-
-
-def getParent(x):
-    if parent[x] == x:
-        return x
-    parent[x] = getParent(parent[x])
-    return parent[x]
-
-
-def unionParent(x, y):
-    a = getParent(x)
-    b = getParent(y)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
-def findParent(x, y):
-    return getParent(x) == getParent(y)
-
-
-# problem solve
-for pair in list(combinations([i for i in range(N)], 2)):
-    length = (vertex[pair[0]][0] - vertex[pair[1]][0]) ** 2 + (vertex[pair[0]][1] - vertex[pair[1]][1]) ** 2
-    if length >= C:
-        edges.append((pair[0], pair[1], length)) # 메모리 초과 해결!
-
-# kruskal
+N, C = map(int, input().split())
+vertex = []
+for i in range(N):
+    x, y = map(int, input().split())
+    vertex.append((i, x, y))
+chk = [False] * N
+pq = []
+cnt = 0
 answer = 0
-edges.sort(key=lambda x: x[2])
-for edge in edges:
-    if not findParent(edge[0], edge[1]):
-        unionParent(edge[0], edge[1])
-        answer += edge[2]
+# problem solve
+# prim-algorithm
+chk[0] = True
+for i in range(1, len(vertex)):
+    cost = (vertex[0][1] - vertex[i][1]) ** 2 + (vertex[0][2] - vertex[i][2]) ** 2
+    if cost >= C:
+        heapq.heappush(pq, (cost, vertex[i][0]))
+
+while cnt < N - 1:
+    if not pq:
+        break # C보다 비용이 큰 간선만 추가되므로, N-1개를 선택하기 전에 pq가 empty일 가능성 있음
+    cand = heapq.heappop(pq)
+    if chk[cand[1]]:
+        continue
+    chk[cand[1]] = True
+    cnt += 1
+    answer += cand[0]
+
+    for i in range(0, len(vertex)):
+        if i != cand[1] and not chk[i]:
+            cost = (vertex[cand[1]][1] - vertex[i][1]) ** 2 + (vertex[cand[1]][2] - vertex[i][2]) ** 2
+            if cost >= C:
+                heapq.heappush(pq, (cost, i))
+
+if cnt < N - 1:
+    answer = -1
+
 print(answer)
