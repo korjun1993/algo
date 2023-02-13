51e934a3a6af383fbacc6f792988aa2712bb3932
korjun1993 <corojoon93@naver.com>
2023-02-13 18:14:37 +0900

    [추가] 프로그래머스 - 기지국설치

---
+++ src/programmers/Solution_기지국설치.java
@@ -0,0 +1,40 @@
+package programmers;
+
+public class Solution_기지국설치 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.solution(16, new int[]{9}, 2);
+        System.out.println(answer);
+    }
+}
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+
+        // 첫 번째 구간에 대한 기지국 개수 구하기
+        int left = 1;
+        int right = stations[0] - w - 1;
+        answer += getStationNumber(left, right, w);
+
+        // 중간 구간에 대한 기지국 개수 구하기
+        for (int i = 0; i < stations.length - 1; i++) {
+            left = stations[i] + w + 1;
+            right = stations[i + 1] - w - 1;
+            answer += getStationNumber(left, right, w);
+        }
+
+        // 마지막 구간에 대한 기지국 개수 구하기
+        left = stations[stations.length - 1] + w + 1;
+        right = n;
+        answer += getStationNumber(left, right, w);
+        return answer;
+    }
+
+    public int getStationNumber(int left, int right, int w) {
+        int bound = 2 * w + 1;
+        int len = right - left + 1;
+        if (len == 0) return 0;
+        return (int) Math.ceil((double) len / bound);
+    }
+}
